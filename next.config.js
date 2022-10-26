9348078de01449a198ae8c54f9dafb5b28215bdf
xkvbikx <xkvbikx@gmail.com>
2022-10-26 13:25:18 +0200

    update env, distDir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+        NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
+        NEXT_PUBLIC_BACKEND_ENDPOINT: process.env.NEXT_PUBLIC_BACKEND_ENDPOINT,
+    },
+    distDir: "build",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
