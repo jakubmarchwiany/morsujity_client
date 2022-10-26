a5741059f22ef34ec90e810ff91fad6b1b91b024
xkvbikx <xkvbikx@gmail.com>
2022-10-26 13:21:52 +0200

    add blog pages

--- pages/blog/3.tsx
+++ pages/blog/3.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
 
 function post3() {
-  return <div>post3</div>;
+    return (
+        <Box mb={30}>
+            <Typography variant="h2" mt={5} component="h1" textAlign="center">
+                Motywacja
+            </Typography>
+            <Typography mt={10} mb={5} variant="body1" mx={10}></Typography>
+        </Box>
+    );
 }
 
 export default post3;
