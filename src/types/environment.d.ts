184fc4a79b06f8ab3262b0635ba458457124a0a4
xkvbikx <xkvbikx@gmail.com>
2022-10-26 13:19:18 +0200

    update names

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,10 +1,10 @@
 export {};
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      //running type
-      NEXT_PUBLIC_ENV: string;
-      NEXT_PUBLIC_DEV_API_ENDPOINT: string;
+    namespace NodeJS {
+        interface ProcessEnv {
+            //running type
+            NEXT_PUBLIC_ENV: string;
+            NEXT_PUBLIC_BACKEND_ENDPOINT: string;
+        }
     }
-  }
 }
--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -1,9 +1,9 @@
 import { cleanEnv, str } from "envalid";
 
 function validateEnv() {
-  cleanEnv(process.env, {
-    NEXT_PUBLIC_ENV: str(),
-    NEXT_PUBLIC_DEV_API_ENDPOINT: str(),
-  });
+    cleanEnv(process.env, {
+        NEXT_PUBLIC_ENV: str(),
+        NEXT_PUBLIC_BACKEND_ENDPOINT: str(),
+    });
 }
 export default validateEnv;
