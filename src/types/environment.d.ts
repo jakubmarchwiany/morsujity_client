6da6721197e5a0a6bc96de17d420050de19beedb
kvbik <xkvbikx@gmail.com>
2022-11-14 00:48:20 +0100

    update
    - change name _ENDPOINT -> URL
    - add USER_APP_URL

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -2,9 +2,9 @@ export {};
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            //running type
             NEXT_PUBLIC_ENV: string;
-            NEXT_PUBLIC_BACKEND_ENDPOINT: string;
+            NEXT_PUBLIC_USER_APP_URL: string;
+            NEXT_PUBLIC_BACKEND_URL: string;
         }
     }
 }
--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -3,7 +3,8 @@ import { cleanEnv, str } from "envalid";
 function validateEnv() {
     cleanEnv(process.env, {
         NEXT_PUBLIC_ENV: str(),
-        NEXT_PUBLIC_BACKEND_ENDPOINT: str(),
+        NEXT_PUBLIC_USER_APP_URL: str(),
+        NEXT_PUBLIC_BACKEND_URL: str(),
     });
 }
 export default validateEnv;
