7550fb5bcb456196e99e6cec537e755997e29c62
kvbik <xkvbikx@gmail.com>
2022-11-14 01:03:39 +0100

    update - add backend url - add toast loading - add custom error handler

--- src/utils/fetches.ts
+++ src/utils/fetches.ts
@@ -1,42 +1,61 @@
-export async function getFetch<T>(url: string): Promise<T & { message: string }> {
+import toast from "react-hot-toast";
+
+const END_POINT = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export async function getFetch<T>(
+    url: string,
+    options?: { customError?: boolean }
+): Promise<T & { message: string }> {
     return new Promise((resolve, reject) => {
-        fetch(url, {
+        const toastId = toast.loading("Ładowanie...");
+        fetch(END_POINT + url, {
             method: "GET",
+            credentials: "include",
             headers: { "Content-Type": "application/json" },
         })
             .then(async (response) => {
                 const data = (await response.json()) as T & { message: string };
                 if (response.ok) {
+                    toast.success(data.message, { id: toastId });
                     resolve(data);
                 } else {
-                    reject(data);
+                    toast.error(data.message, { id: toastId });
+                    if (options?.customError) reject(data);
                 }
             })
             .catch((error) => {
-                console.log(error);
-                reject(error);
+                toast.error(error.message, { id: toastId });
+                if (options?.customError) reject(error);
             });
     });
 }
 
-export async function postFetch<T>(body: object, url: string): Promise<T & { message: string }> {
+export async function postFetch<T>(
+    body: object,
+    url: string,
+    options?: { customError?: boolean }
+): Promise<T & { message: string }> {
     return new Promise((resolve, reject) => {
-        fetch(url, {
+        const toastId = toast.loading("Ładowanie...");
+        fetch(END_POINT + url, {
             method: "POST",
+            credentials: "include",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(body),
         })
             .then(async (response) => {
                 const data = (await response.json()) as T & { message: string };
                 if (response.ok) {
+                    toast.success(data.message, { id: toastId });
                     resolve(data);
                 } else {
-                    reject(data);
+                    toast.error(data.message, { id: toastId });
+                    if (options?.customError) reject(data);
                 }
             })
             .catch((error) => {
-                console.log(error);
-                reject(error);
+                toast.error(error.message, { id: toastId });
+                if (options?.customError) reject(error);
             });
     });
 }
