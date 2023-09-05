8e31af132d3cf9a63137590a9c8360524cd0519c
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-09-05 13:42:37 +0200

    update blog pages

--- src/pages/blog/1.tsx
+++ src/pages/blog/1.tsx
@@ -1,13 +1,107 @@
-import { Box, Typography } from "@mui/material";
+import { Container, Stack, Typography } from "@mui/material";
+
+import Head from "next/head";
 
 function post1() {
     return (
-        <Box mb={30}>
-            <Typography variant="h2" mt={5} component="h1" textAlign="center">
-                Aplikacja
-            </Typography>
-            <Typography variant="body1" mx={10}></Typography>
-        </Box>
+        <>
+            <Head>
+                <title>O aplikacji</title>
+                <meta name="description" content="Opis aplikacji" />
+                <link rel="canonical" href="/blog/1" />
+            </Head>
+            <Container component="main">
+                <Stack my={{ xs: 3, md: 5 }}>
+                    <Typography variant="h3" gutterBottom textAlign={"center"}>
+                        Witaj w Aplikacji do Morsowania!
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={{ xs: 2, md: 3 }}>
+                        Rejestracja i Logowanie
+                    </Typography>
+                    <Typography paragraph>
+                        Aby rozpocząć korzystanie z aplikacji, zarejestruj się poprzez podanie
+                        swojego adresu e-mail, nazwy użytkownika i hasła. Po rejestracji możesz
+                        także logować się do swojego konta.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Profil Użytkownika
+                    </Typography>
+                    <Typography paragraph>
+                        Personalizuj swój profil, dodając zdjęcie profilowe, opis oraz informacje o
+                        swoich osiągnięciach w morsowaniu.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Zapisywanie Morsów i Zimnych Pryszniców
+                    </Typography>
+                    <Typography paragraph>
+                        Aplikacja pozwala na rejestrowanie swoich morsów i zimnych pryszniców.
+                        Możesz wprowadzać datę, czas, lokalizację oraz dodatkowe notatki. Oceniaj
+                        swoje doświadczenia.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Tworzenie Grup
+                    </Typography>
+                    <Typography paragraph>
+                        Stwórz grupy tematyczne, na przykład na podstawie lokalizacji lub
+                        preferowanych miejsc do morsowania. Grupy pomagają w organizacji wspólnych
+                        wyjść do wody.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Dołączanie do Grup
+                    </Typography>
+                    <Typography paragraph>
+                        Przeglądaj dostępne grupy i dołącz do tych, które cię zainteresują. To
+                        ułatwia nawiązywanie kontaktów z osobami o podobnych zainteresowaniach.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Umawianie się na Wspólne Morsy
+                    </Typography>
+                    <Typography paragraph>
+                        Aplikacja oferuje funkcję umawiania się na wspólne morsy z innymi
+                        użytkownikami lub w ramach grup. Ustalaj daty, godziny i miejsca spotkań
+                        oraz komunikuj się w ramach aplikacji.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Zdobywanie Rang
+                    </Typography>
+                    <Typography paragraph>
+                        Zdobywaj rangi w oparciu o swoją aktywność i osiągnięcia. Rangi motywują do
+                        udziału w społeczności i aktywności w aplikacji.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Powiadomienia i Aktualności
+                    </Typography>
+                    <Typography paragraph>
+                        Otrzymuj powiadomienia o wydarzeniach, aktualnościach w grupach i swoich
+                        osiągnięciach, aby być na bieżąco.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Bezpieczeństwo
+                    </Typography>
+                    <Typography paragraph>
+                        Aplikacja dba o bezpieczeństwo użytkowników, umożliwiając blokowanie lub
+                        zgłaszanie nieodpowiednich treści lub użytkowników.
+                    </Typography>
+
+                    <Typography variant="h4" gutterBottom mt={1}>
+                        Statystyki i Historia Morsów
+                    </Typography>
+                    <Typography paragraph>
+                        Śledź swoje postępy, oglądając statystyki morsów, zimnych pryszniców i
+                        innych aktywności na swoim profilu. Przeglądaj swoją historię morsów.
+                    </Typography>
+                </Stack>
+            </Container>
+        </>
     );
 }
 
--- src/pages/blog/2.tsx
+++ src/pages/blog/2.tsx
@@ -1,80 +1,120 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Checkbox, Container, List, ListItem, Stack, Typography } from "@mui/material";
+import Head from "next/head";
 
 function post2() {
     return (
-        <Box mb={30}>
-            <Typography variant="h2" mt={5} component="h1" textAlign="center">
-                Jak zacząć morsowanie. Poradnik dla początkujących
-            </Typography>
-            <Typography mt={10} mb={5} variant="body1" mx={10}>
-                <div>
-                    <Typography variant="h6">ZASADY BEZPIECZNEGO ZIMOWEGO PŁYWANIA</Typography>
-                    <div>
-                        &bull; Uprzednia adaptacja do niższych temperatur(np. za pomocą zimnych
-                        prysznic&oacute;w).
-                    </div>
-                    <div>
-                        &bull; Spokojna rozgrzewka trwająca od 10 do 15 minut. Ma nas rozgrzać, ale
-                        nie spocić.
-                    </div>
-                    <div>
-                        &bull; Rozbieramy się zaczynając od dołu do g&oacute;ry, ubieramy odwrotnie
-                        po wytarciu całego ciała.
-                    </div>
-                    <div>
-                        &bull; Na wejście do wody możemy zabrać obuwie(od but&oacute;w neoprenowych
-                        po zwykłe klapki). Kwestia odłamk&oacute;w szkła/poślizgnięcia się na
-                        lodzie.
-                    </div>
-                    <div>
-                        &bull; Czas kąpieli uzależniony jest od naszego stażu, warunk&oacute;w
-                        pogodowych i aktualnego samopoczucia, nie przesadzamy. To samo odnosi się do
-                        częstotliwości morsowania.
-                    </div>
-                    <div>
-                        &bull; Przed samą kąpielą, tak jak przed każdym treningiem, trzeba być
-                        wypoczętym, odżywionym i nawodnionym oraz nie wolno korzystać z
-                        jakichkolwiek używek takich, jak papierosy czy alkohol. Nap&oacute;j z
-                        procentami dodatkowo wychładza organizm, obniża ciśnienie krwi oraz może
-                        powodować skurcze uniemożliwiające ruch w wodzie.
-                    </div>
-                </div>
-                <div>
-                    <Typography variant="h6">
-                        DLA KOGO NIE JEST WSKAZANA ZIMNA TERMOGENEZA
+        <>
+            <Head>
+                <title>Poradnik</title>
+                <meta name="description" content="Poradnik morsa" />
+                <link rel="canonical" href="/blog/2" />
+            </Head>
+            <Container component="main">
+                <Stack my={{ xs: 3, md: 5 }}>
+                    <Typography variant="h3" component="h1" textAlign="center">
+                        Jak zacząć morsowanie.
+                        <br /> Poradnik dla początkujących
                     </Typography>
 
-                    <div>&bull; Dla os&oacute;b z bardzo słabą odpornością.</div>
-                    <div>&bull; Dla os&oacute;b z infekcją dr&oacute;g moczowych.</div>
-                    <div>&bull; Dla os&oacute;b, kt&oacute;re mają chore nerki.</div>
-                    <div>
-                        &bull; Dla os&oacute;b z wyczerpanymi nadnerczami i zaburzoną osią
-                        podwzg&oacute;rze-przysadka-nadnercza.
-                    </div>
-                    <div>&bull; Dla os&oacute;b z dużą niedoczynnością tarczycy.</div>
-                    <div>&bull; Dla os&oacute;b z niewydolnością krążeniową.</div>
-                </div>
-                <div>
-                    <Typography variant="h6">CO TRZEBA ZABRAĆ</Typography>
+                    <Typography variant="h5" mt={{ xs: 3, md: 5 }}>ZASADY BEZPIECZNEGO ZIMOWEGO PŁYWANIA</Typography>
+                    <List>
+                        <ListItem>
+                            <Checkbox checked /> Uprzednia adaptacja do niższych temperatur (np. za
+                            pomocą zimnych pryszniców).
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Spokojna rozgrzewka trwająca od 10 do 15 minut. Ma
+                            nas rozgrzać, ale nie spocić.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Rozbieramy się zaczynając od dołu do góry, ubieramy
+                            odwrotnie po wytarciu całego ciała.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Na wejście do wody możemy zabrać obuwie (od butów
+                            neoprenowych po zwykłe klapki) - kwestia odłamków szkła/poślizgnięcia
+                            się na lodzie.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Czas kąpieli uzależniony jest od naszego stażu,
+                            warunków pogodowych i aktualnego samopoczucia, nie przesadzamy. To samo
+                            odnosi się do częstotliwości morsowania.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Przed samą kąpielą, tak jak przed każdym
+                            treningiem, trzeba być wypoczętym, odżywionym i nawodnionym oraz nie
+                            wolno korzystać z jakichkolwiek używek takich, jak papierosy czy
+                            alkohol. Napój z procentami dodatkowo wychładza organizm, obniża
+                            ciśnienie krwi oraz może powodować skurcze uniemożliwiające ruch w
+                            wodzie.
+                        </ListItem>
+                    </List>
 
-                    <div></div>
-                    <div>&bull; Ręcznik</div>
-                    <div>&bull; Bieliznę na zmianę</div>
-                </div>
-                <div>
-                    <Typography variant="h6">CO MOŻNA ZABRAĆ</Typography>
+                    <Typography variant="h5">
+                        DLA KOGO NIE JEST WSKAZANA ZIMNA TERMOGENEZA
+                    </Typography>
+                    <List>
+                        <ListItem>
+                            <Checkbox checked /> Dla osób z bardzo słabą odpornością.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Dla osób z infekcją dróg moczowych.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Dla osób, które mają chore nerki.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Dla osób z wyczerpanymi nadnerczami i zaburzoną
+                            osią podwzgórze-przysadka-nadnercza.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Dla osób z dużą niedoczynnością tarczycy.
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Dla osób z niewydolnością krążeniową.
+                        </ListItem>
+                    </List>
 
-                    <div>&bull; Rękawiczki(mogą być neoprenowe)</div>
-                    <div>&bull; Obuwie do wody i(może być neoprenowe)</div>
-                    <div>&bull; Czapkę</div>
-                    <div>&bull; Karimatę pod nogi</div>
-                    <div>&bull; Szalik/komin/kominiarkę</div>
-                    <div>&bull; Termos z gorącą kawą/herbatą</div>
-                    <div>&bull; Pas rozgrzewający na nerki&nbsp;</div>
-                    <div>&bull; Szlafrok</div>
-                </div>
-            </Typography>
-        </Box>
+                    <Typography variant="h5">CO TRZEBA ZABRAĆ</Typography>
+                    <List>
+                        <ListItem>
+                            <Checkbox checked /> Ręcznik
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Bieliznę na zmianę
+                        </ListItem>
+                    </List>
+
+                    <Typography variant="h5">CO MOŻNA ZABRAĆ</Typography>
+                    <List>
+                        <ListItem>
+                            <Checkbox checked /> Rękawiczki (mogą być neoprenowe)
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Obuwie do wody i (może być neoprenowe)
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Czapkę
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Karimatę pod nogi
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Szalik/komin/kominiarkę
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Termos z gorącą kawą/herbatą
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Pas rozgrzewający na nerki
+                        </ListItem>
+                        <ListItem>
+                            <Checkbox checked /> Szlafrok
+                        </ListItem>
+                    </List>
+                </Stack>
+            </Container>
+        </>
     );
 }
 
